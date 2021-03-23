e1880e8f52ef27be3800c0da56ac5bf7b46d63e2
Samidha09 <samidha.verma1996@gmail.com>
2021-03-23 11:57:55 +0530

    updated experiment stats

---
+++ Assignments/Benchmarking/NodeClassification/exp2.py
@@ -0,0 +1,21 @@
+import os
+
+model_type = ['SAGE', 'GCN']
+aggr = 'max'
+num_layers = [2, 3]
+hidden_dim = 256
+epochs = [100, 200, 300, 400, 500, 600, 700]
+
+#print(model_type, num_layers, hidden_dim, epochs, aggr)
+os.system("mkdir plot_results")
+os.system("mkdir ./plot_results/SAGE")
+os.system("mkdir ./plot_results/GCN")
+
+for model in model_type:
+    for epoch in epochs:
+        if model == 'SAGE':
+            os.system("python3 train.py --model_type={} --num_layers={} --hidden_dim={} --epochs={} --aggregator={}".format(
+                model, num_layers[1], hidden_dim, epoch, aggr))
+        else:
+            os.system("python3 train.py --model_type={} --num_layers={} --hidden_dim={} --epochs={}".format(
+                      model, num_layers[0], hidden_dim, epoch))
---
+++ Assignments/Benchmarking/NodeClassification/exp3.py
@@ -0,0 +1,21 @@
+import os
+
+model_type = ['SAGE', 'GCN']
+aggr = 'max'
+num_layers = [2, 3]
+hidden_dim = [64, 128, 256, 512]
+epochs = 300
+
+#print(model_type, num_layers, hidden_dim, epochs, aggr)
+os.system("mkdir dim_results")
+os.system("mkdir ./dim_results/SAGE")
+os.system("mkdir ./dim_results/GCN")
+
+for model in model_type:
+    for dim in hidden_dim:
+        if model == 'SAGE':
+            os.system("python3 train.py --model_type={} --num_layers={} --hidden_dim={} --epochs={} --aggregator={}".format(
+                model, num_layers[1], dim, epochs, aggr))
+        else:
+            os.system("python3 train.py --model_type={} --num_layers={} --hidden_dim={} --epochs={}".format(
+                      model, num_layers[0], dim, epochs))
